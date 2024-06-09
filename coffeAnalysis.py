b6453f0ea9ff8af8c3e15d7ba88266d1ff1db96a
Samira Bobes <samirabobes@gmail.com>
2024-06-09 20:13:13 -0300

    Functions definition

--- coffeAnalysis.py
+++ coffeAnalysis.py
@@ -1,26 +1,37 @@
 import pandas as pd
 
 #Extract
-coffeeProduction = pd.read_csv("./Coffee_production.csv", delimiter=',', encoding='unicode_escape')
-coffeeConsumtion = pd.read_csv("./Coffee_domestic_consumption.csv", delimiter=',', encoding='unicode_escape')
+def extractData(path):
+    return pd.read_csv(path, delimiter=',', encoding='unicode_escape')
+
+coffeeProduction = extractData("./Coffee_production.csv")
+coffeeConsumtion = extractData("./Coffee_domestic_consumption.csv")
 
 #Transform
-def bagsToMTn(x):
-    # original unit is in thousand bags and each bag is of 60kg. Convertion to tones
-    return (x*60)/1000000
+def bagsToMTn(bags):
+    # original unit is in thousand bags and each bag is of 60kg. 
+    kgPerBag = 60
+    #Convertion to thousand tones
+    umTransformation = 1000000
+    return (bags*kgPerBag)/umTransformation
+def dataCleaning(dataFrame, UnitColumn, Country):
+    # Delete rows with zero values
+    cleanDataFrame = dataFrame[(dataFrame != 0).all(axis=1)]
+    # Convert from bags units to thousand tones
+    convertDataFrame = cleanDataFrame.copy()  # Explicit copy here
+    convertDataFrame[UnitColumn] = convertDataFrame[UnitColumn].apply(bagsToMTn)
+    # Select top 10 rows ordered by UnitColumn
+    topTenDataFrame = convertDataFrame[[Country, UnitColumn]].sort_values(by=[UnitColumn], ascending=False).head(10)
+    return topTenDataFrame
 
-cleanCoffeeProduction = coffeeProduction.copy()
-cleanCoffeeProduction = cleanCoffeeProduction[(cleanCoffeeProduction != 0).all(axis=1)]
-cleanCoffeeProduction['Total_production'] = cleanCoffeeProduction['Total_production'].apply(bagsToMTn)
-top_production = cleanCoffeeProduction[['Country', 'Total_production']].sort_values(by=['Total_production'], ascending=False).head(10)
+top_production = dataCleaning(coffeeProduction, 'Total_production', 'Country')
+top_consumption = dataCleaning(coffeeConsumtion, 'Total_domestic_consumption', 'Country')
+consumptionVSproduction = top_production.merge(top_consumption, on='Country')
 
-cleanCoffeeConsumption = coffeeConsumtion.copy()
-cleanCoffeeConsumption = cleanCoffeeConsumption[(cleanCoffeeConsumption != 0).all(axis=1)]
-cleanCoffeeConsumption['Total_domestic_consumption'] = cleanCoffeeConsumption['Total_domestic_consumption'].apply(bagsToMTn)
-top_consumtion = cleanCoffeeConsumption[['Country', 'Total_domestic_consumption']].sort_values(by=['Total_domestic_consumption'], ascending=False).head(10)
+#Load
+load = pd.ExcelWriter('Coffee-Analysis-SB.xlsx') 
+top_production.to_excel(load, sheet_name='Top-Coffee-Production')
+top_consumption.to_excel(load, sheet_name='Top-Coffee-Consumption')
+consumptionVSproduction.to_excel(load, sheet_name='Production-vs-Consumption')
 
-print(top_production.head(10))
-print(top_consumtion.head(10))
-
-consumptionVSproduction = top_production.merge(top_consumtion, on='Country')
-print(consumptionVSproduction)
+load.close()
